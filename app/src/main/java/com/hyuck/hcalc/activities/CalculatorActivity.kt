0b39d2aeaf2772c8123116cf431fa6082e6c7f33
Hyuck9 <lhg1304@gmail.com>
2020-11-16 18:46:36 +0900

    Digit Button Layout

---
+++ app/src/main/java/com/hyuck/hcalc/activities/CalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.hyuck.hcalc.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hyuck.hcalc.R
+
+class CalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+    }
+}
