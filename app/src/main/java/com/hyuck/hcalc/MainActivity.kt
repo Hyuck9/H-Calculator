bbf499c558f84973906aa01a42c680959183ad60
Hyuck9 <lhg1304@gmail.com>
2020-11-17 18:58:40 +0900

    test

--- app/src/main/java/com/hyuck/hcalc/MainActivity.kt
+++ app/src/main/java/com/hyuck/hcalc/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.hyuck.hcalc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hyuck.hcalc.activities.CalculatorActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, CalculatorActivity::class.java))
+        finish()
     }
 }
